567278d0c1271095bbacdb7c71fffbfb765ab39e
Rummut <kzantip@gmail.com>
2023-08-13 19:07:04 +0300

    add two tasks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,51 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { FeedbackResults } from './feedback/FeedbackResults';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+    total: 0,
+    percentage: 0,
+  };
+
+  handleClick = type => {
+    this.setState(prevState => {
+      return {
+        [type]: prevState[type] + 1,
+        total: this.countTotalFeedback(),
+        percentage: this.countPositiveFeedbackPercentage()
+      };
+    });
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state
+    return good + neutral + bad
+  }
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state
+    return Number.parseInt((good/this.countTotalFeedback())*100)
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Please leave feedback</h1>
+
+        <button type="button" onClick={() => {this.handleClick("good")}}>
+          Good
+        </button>
+        <button type="button" onClick={() => {this.handleClick("neutral")}}>
+          Neutral
+        </button>
+        <button type="button" onClick={() => {this.handleClick("bad")}}>
+          Bad
+        </button>
+        <FeedbackResults state={this.state} ></FeedbackResults>
+      </>
+    );
+  }
+}
---
+++ src/components/feedback/FeedbackResults.jsx
@@ -0,0 +1,12 @@
+export const FeedbackResults = ({ state: { good, neutral, bad, total, percentage } }) => {
+  return (
+    <>
+      <h2>Statistic</h2>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>Total: {total} </p>
+      <p>Positive feedback: {percentage}%</p>
+    </>
+  );
+};
